74b72d6d2d68a5700aa5e2b5bb20642443dfb401
Maheboob7699 <murdimaheboob@gmail.com>
2024-08-03 08:18:19 -0900

    Jarvis_Website

---
+++ Assets/index.js
@@ -0,0 +1,35 @@
+let broker1=document.querySelector("#class-1");
+broker1.addEventListener("click",function(){
+    broker1.classList.toggle("blue-color");
+})
+
+let broker2=document.querySelector("#class-2");
+broker2.addEventListener("click",function(){
+    broker2.classList.toggle("blue-color");
+})
+
+let broker3=document.querySelector("#class-3");
+broker3.addEventListener("click",function(){
+    broker3.classList.toggle("blue-color");
+})
+
+let broker4=document.querySelector("#class-4");
+broker4.addEventListener("click",function(){
+    broker4.classList.toggle("blue-color");
+})
+
+let broker5=document.querySelector("#class-5");
+broker5.addEventListener("click",function(){
+    broker5.classList.toggle("blue-color");
+})
+
+let broker6=document.querySelector("#class-6");
+broker6.addEventListener("click",function(){
+    broker6.classList.toggle("blue-color");
+})
+
+
+let getBtn=document.querySelector("#start-button");
+getBtn.addEventListener("click",function(){
+
+})
